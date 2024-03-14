be011d9d69acfc9cec21b0f03f3c6688adf5fdb7
datairawan <adeirawan.growth@gmail.com>
2024-03-14 12:59:13 +0700

    add cache

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import diskcache as dc
+cache = dc.Cache("cache")
 
 st.markdown("Ade Ganteng")
+
+file_key = "parquet_data"
+if file_key in cache:
+    print(f"File sudah ada di cache..")
+else:
+    print("adding to cache..")
+    with cache as c:
+        c.set("parquet_data", "sumpah dah")
+
+with cache as c:
+    data_cached = c.get("parquet_data")
+
+st.markdown(data_cached)
